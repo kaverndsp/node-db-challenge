9f565c39ec2d20d3a2ff854e1587331f15813951
kaverndsp <kaverndsp@users.noreply.github.com>
2020-03-20 10:18:47 -0500

    server is now running

--- knexfile.js
+++ knexfile.js
@@ -4,41 +4,16 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true, // needed for sqlite
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: './data/projects.db3'
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      directory: './data/migrations'
     },
-    pool: {
-      min: 2,
-      max: 10
+    seeds: {
+      directory: './data/seeds'
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
 
 };
