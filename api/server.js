a92a506e7a8d2d217f288c279b5c2805f615e7d9
kaverndsp <kaverndsp@users.noreply.github.com>
2020-03-20 12:38:02 -0500

    endpoints are functional. I think mvp has been reached

--- api/server.js
+++ api/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const projectRouter = require('../routes/projectRouter.js');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/projects', projectRouter);
 
 server.get("/", (req, res) => {
     res.send('Server is running!')
